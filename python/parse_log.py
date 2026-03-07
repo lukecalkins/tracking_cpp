Fixed bug with converting b_sigma to radians. UKF still diverging

python/parse_log.py
@@ -54,11 +54,11 @@ def read_log(log_file):
 
 if __name__ == '__main__':
     logdir = '../data/'
-    logfile = 'multi_sensor_UKF_middle_split_bsigma_2_asigma_0_1.txt'
-    video_filename = "../data/multi_sensor_UKF_middle_split_bsigma_2_asigma_0_1_nstd_2.gif"
-    title = "Unnscented Kalman Filter with 2 bearing-only sensors"
+    logfile = 'multi_sensor_ISAM_middle_split_bsigma_2_asigma_0_1.txt'
+    video_filename = "../data/multi_sensor_ISAM_middle_split_bsigma_2_asigma_0_1_nstd_2.gif"
+    title = "ISAM2 algo via GTSAM with 2 bearing-only sensors"
     # title = "Extended Kalman Filter wiith 2 bearing-only sensors"
-    fps=100
+    fps=10
     full_file = os.path.join(logdir, logfile)
     measurements0, measurements1, target_states, ownship0_states, ownship1_states, target_belief_states, cov_belief_states = read_log(full_file)
     print("Mean of measurments 0: ", np.mean(measurements0)*180./np.pi, "Std: ", np.std(measurements0)*180./np.pi)